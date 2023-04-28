49ce9aeeac4202683ecec4ec77915caa9a3be831
Andrei Sushkou <AndreySushkov@My-Air.local>
2023-04-28 13:51:16 +0300

    Initial project setup

---
+++ Animated loader/Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animated loader
+//
+//  Created by Andrei Sushkou on 28.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
